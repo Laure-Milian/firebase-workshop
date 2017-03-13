2ad2d4fb77f62b8f73ec10c668385a35c28583cc
Laure-Milian <laure.milian@gmail.com>
2017-03-13 16:48:30 +0100

    fix affichage liste en double

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-(function(){
-	var formulaire = document.getElementById("mon-formulaire");
-	var db = firebase.database();
-	var list = $("#my-list");
+var db = firebase.database();
+var formulaire = document.getElementById("mon-formulaire");
+var list = $("#my-list");
 
-	var query = db.ref("items").orderByKey();
-	query.once("value")
-		.then(function(snapshot) {
-			snapshot.forEach(function(childSnapshot) {
-				var key = childSnapshot.key;
-				var childData = childSnapshot.val();
-				list.append("<li>" + childData.content + "</li>");
-			})
-		})
+//var query = db.ref("items").orderByKey();
 
-	formulaire.addEventListener('submit', function(event){
-	    event.preventDefault();
-	    var task = document.getElementById("task");
-	    console.log(task);
-		db.ref('/items/').push({
-			createdAt: Date.now(),
-		 	content: task.value,
-			status: "à faire"
-		})
-	});
+// Afficher tous les items
+// query.once("value")
+// 	.then(function(snapshot) {
+// 		snapshot.forEach(function(childSnapshot) {
+// 			var key = childSnapshot.key;
+// 			var childData = childSnapshot.val();
+// 			list.append("<li>" + childData.content + "</li>");
+// 		})
+// 	})
 
-	db.ref('/items').on('child_added', function(snapshot) {
-	   		var key = snapshot.key;
-	   		var childData = snapshot.val();
-			list.append("<li>" + childData.content + "</li>");
+// Ajouter une tâche
+formulaire.addEventListener('submit', function(event){
+    event.preventDefault();
+    var task = document.getElementById("task");
+	db.ref('/items/').push({
+		createdAt: Date.now(),
+	 	content: task.value,
+		status: "à faire"
 	})
+});
+
+// Mettre à jour la liste quand une nouvelle tâche est ajoutée
+db.ref('/items').on('child_added', function(snapshot) {
+   		var key = snapshot.key;
+   		var childData = snapshot.val();
+		list.append("<li>" + childData.content + "</li>");
+});
 	
 
-})();
