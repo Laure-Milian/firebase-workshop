f1ba50361e1058c97ee24ff2b091b10adb86693e
Laure-Milian <laure.milian@gmail.com>
2017-03-13 16:19:06 +0100

    affiche et ajoute tasks

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 (function(){
-  var formulaire = document.getElementById("mon-formulaire");
+	var formulaire = document.getElementById("mon-formulaire");
+	var db = firebase.database();
+	var list = $("#my-list");
 
-  formulaire.addEventListener('submit', function(event){
-    event.preventDefault();
-    var task = document.getElementById("task");
-  });
+	formulaire.addEventListener('submit', function(event){
+	    event.preventDefault();
+	    var task = document.getElementById("task");
+	    console.log(task);
+		db.ref('/items/').push({
+			createdAt: Date.now(),
+		 	content: task.value,
+			status: "à faire"
+		})
+	});
+
+	db.ref('/items').on('value', function(snapshot) {
+	 	snapshot.forEach(function(childSnapshot) {
+	   		var key = childSnapshot.key;
+	   		var childData = childSnapshot.val();
+			list.append("<li>" + childData.content + "</li>");
+	   	})
+	})
+	
 
 })();
