af15faa0f4cd00a4721838ffdc8788be6327684f
Laure-Milian <laure.milian@gmail.com>
2017-03-13 16:28:48 +0100

    affichage du dernier element au moment de l'ajout

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 	var db = firebase.database();
 	var list = $("#my-list");
 
+	var query = db.ref("items").orderByKey();
+	query.once("value")
+		.then(function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				var key = childSnapshot.key;
+				var childData = childSnapshot.val();
+				list.append("<li>" + childData.content + "</li>");
+			})
+		})
+
 	formulaire.addEventListener('submit', function(event){
 	    event.preventDefault();
 	    var task = document.getElementById("task");
@@ -14,12 +24,10 @@
 		})
 	});
 
-	db.ref('/items').on('value', function(snapshot) {
-	 	snapshot.forEach(function(childSnapshot) {
-	   		var key = childSnapshot.key;
-	   		var childData = childSnapshot.val();
+	db.ref('/items').on('child_added', function(snapshot) {
+	   		var key = snapshot.key;
+	   		var childData = snapshot.val();
 			list.append("<li>" + childData.content + "</li>");
-	   	})
 	})
 	
 
